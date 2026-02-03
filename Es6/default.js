fb34883e1097019c47be5f8dee9d25e1c19f19f3
SabbirReturn <mdsabbirhasan39@gmail.com>
2026-02-03 17:16:58 +0600

    again

--- Es6/default.js
+++ Es6/default.js
@@ -8,7 +8,7 @@
 //    console.log(sum)
 // }
 // // console.log(sum(2,10,11,12,13))
-function sum (num1, num2, ...numbers){
+function sum (num1, num2=3, ...numbers){
     let total = num1 + num2;
     let sum = 0;
     for(let num of numbers){
@@ -17,4 +17,11 @@ function sum (num1, num2, ...numbers){
     let full = total + sum
     return full
 }
-console.log(sum(11,1,2,3,4,5,5,))
+// console.log(sum(11,1,2,3,4,5,5,))
+
+
+function fullName(name1, name2){
+    let name = name1 + name2;
+    return name
+}
+console.log('sabbir','Hasan')
