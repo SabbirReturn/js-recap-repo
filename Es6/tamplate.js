195b3ab3a9513fc7ac5b9e5085a751fc6c2ccbf3
SabbirReturn <mdsabbirhasan39@gmail.com>
2026-02-05 23:21:36 +0600

    again

--- Es6/tamplate.js
+++ Es6/tamplate.js
@@ -1,8 +1,3 @@
-// function sum(num1, num2){
-//     let total = `${num1} equal to ${num2} = ${num1 + num2}`
-//     return total
-// }
-// console.log(sum(12,13))
 function sum(num1, num2){
     let total = `${num1} equal to ${num2} = ${num1 + num2}`
     return total
