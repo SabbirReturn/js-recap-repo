327d45d52b475c2f9e373d4fcdf51aafbc0d8507
SabbirReturn <mdsabbirhasan39@gmail.com>
2026-02-21 03:56:09 +0600

    again

--- More_Es6/map.js
+++ More_Es6/map.js
@@ -12,6 +12,21 @@ let friends = ['Sabbir','Hasan','Robin'];
 let length = friends.map(name => name.length);
 let firstLetter = friends.map(name => name[0])
 let multiplyIndex = numbers.map((num,index)=>num * index)
-console.log(length);
-console.log(firstLetter);
-console.log(multiplyIndex)
+// console.log(length);
+// console.log(firstLetter);
+// console.log(multiplyIndex)
+
+// Reduce
+let sum = numbers.reduce((total,num)=> total + num,0);
+// console.log(sum)
+
+
+let products=[
+    {name:'T-shirt', id: 201, price: 500},
+    {name:'pant', id: 202, price: 700},
+    {name:'Shirt', id: 203, price: 800},
+]
+
+let pay = products.reduce((total, product)=> total + product.price,0);
+console.log(pay);
+
