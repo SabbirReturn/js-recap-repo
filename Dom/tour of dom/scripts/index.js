6684733626033bd4a929b1a09788e85b9e712145
SabbirReturn <mdsabbirhasan39@gmail.com>
2026-08-13 19:57:26 +0600

    tour of dom

---
+++ Dom/tour of dom/scripts/index.js
@@ -0,0 +1,6 @@
+// console.log(document.body.innerText)
+let liCollections = document.getElementsByTagName('li');
+console.log(liCollections)
+for(let li of liCollections){
+    console.log(li.innerText)
+}
