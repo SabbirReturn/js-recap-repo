aafaf42e6c2ba98a44a22c6b0f4ad573668bcb62
SabbirReturn <mdsabbirhasan39@gmail.com>
2026-08-18 03:50:50 +0600

    again

--- Dom/tour of dom/scripts/index.js
+++ Dom/tour of dom/scripts/index.js
@@ -1,9 +1,38 @@
 // console.log(document.body.innerText)
 let liCollections = document.getElementsByTagName('li');
-console.log(liCollections)
+// console.log(liCollections)
 for(let li of liCollections){
-    console.log(li.innerText)
+    // console.log(li.innerText)
 }
 
 let fruitsTitle = document.getElementById('fruitsTitle');
+console.log(fruitsTitle)
 fruitsTitle.innerText = 'Fruits Title change by javascript'
+
+let fruitsItems = document.querySelectorAll('.fruitsContainer ul li');
+// console.log(fruitsItems);
+
+
+let items = document.querySelectorAll('#fruitsTitle + ul li')
+// for(let item of items){
+//     console.log(item.innerText)
+// }
+items.forEach(item=>{
+    // console.log(item.innerText)
+})
+
+
+let fruitsContainer = document.getElementById('fruitsTitle')
+let className = fruitsContainer.getAttribute('class')
+// className('hasan')
+let addClass = fruitsContainer.classList.add('backgroundColor')
+let removeClass = fruitsContainer.classList.remove('home')
+// console.log(fruitsContainer.classList)
+
+fruitsContainer.setAttribute('title','Js change Tool tip')
+let container = document.getElementsByClassName('fruitsContainer')
+// console.log(container[0])
+
+let placeContainer = document.getElementById('placeContainer');
+placeContainer.classList.toggle('active');
+console.log(placeContainer.classList)
