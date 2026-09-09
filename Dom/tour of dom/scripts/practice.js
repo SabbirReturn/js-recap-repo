0d34cca1efb11ddd1efef6e746bd827b714fdaa6
SabbirReturn <mdsabbirhasan39@gmail.com>
2026-09-10 03:41:44 +0600

    again

--- Dom/tour of dom/scripts/practice.js
+++ Dom/tour of dom/scripts/practice.js
@@ -1,2 +1,23 @@
-console.log('Hello Js')
-console.log(document.body)
+// console.log('Hello Js')
+// console.log(document.body)
+
+// Get liCollection
+
+let liCollections = document.getElementsByTagName('li');
+
+for(let li of liCollections){
+    console.log(li.innerText);
+}
+console.log(liCollections);
+
+
+// add class and remove
+let fruitsTitle = document.getElementById('fruitsTitle')
+fruitsTitle.classList.add('color');
+
+// fruitsTitle.style.background = 'red'
+
+fruitsTitle.innerText = 'js changed fruits title';
+
+// Set title
+fruitsTitle.title = 'Fol Khaba?'
