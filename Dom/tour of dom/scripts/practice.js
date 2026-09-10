03fa4da8288bb1c7f00c0a569efc9a70df14c5ec
SabbirReturn <mdsabbirhasan39@gmail.com>
2026-09-10 21:57:07 +0600

    again

--- Dom/tour of dom/scripts/practice.js
+++ Dom/tour of dom/scripts/practice.js
@@ -13,6 +13,10 @@ console.log(liCollections);
 
 // add class and remove
 let fruitsTitle = document.getElementById('fruitsTitle')
+fruitsTitle.classList.add('name')
+let attribute= fruitsTitle.getAttribute('class')
+
+console.log(attribute);
 fruitsTitle.classList.add('color');
 
 // fruitsTitle.style.background = 'red'
@@ -21,3 +25,5 @@ fruitsTitle.innerText = 'js changed fruits title';
 
 // Set title
 fruitsTitle.title = 'Fol Khaba?'
+
+
