581ef134d6fa693f3c3228fac04bf467b7bfc87f
SabbirReturn <mdsabbirhasan39@gmail.com>
2026-09-14 23:36:44 +0600

    again

--- Dom/tour of dom/scripts/append.js
+++ Dom/tour of dom/scripts/append.js
@@ -21,6 +21,10 @@ ul.appendChild(li2);
 // li3.innerText = 'Bandarban'
 // ul.appendChild(li3)
 // mainContainer.appendChild(tourSection);
+// let li3 = document.createElement('li');
+// li3.innerText = 'Bandarban'
+// ul.appendChild(li3)
+// mainContainer.appendChild(tourSection);
 let li3 = document.createElement('li');
 li3.innerText = 'Bandarban'
 ul.appendChild(li3)
