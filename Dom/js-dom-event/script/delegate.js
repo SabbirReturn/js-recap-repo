c7a1e621dbe2853a4080ebbf6f0da46537ed2aa1
SabbirReturn <mdsabbirhasan39@gmail.com>
2026-09-24 19:05:47 +0600

    learn about event object

--- Dom/js-dom-event/script/delegate.js
+++ Dom/js-dom-event/script/delegate.js
@@ -16,3 +16,41 @@ addBtn.addEventListener('click', function(){
     li.innerText = 'New Id Item'
     listContainer.appendChild(li);
 })
+
+document.body.addEventListener('keydown',(event)=>{
+    if(event.key === 'Shift'){
+        console.log('Enter press')
+    }
+})
+
+let inputValue = document.getElementById('inputValue');
+let outPut = document.getElementById('outPut')
+
+inputValue.addEventListener('change',(event)=>{
+    outPut.innerText = event.target.value
+    event.target.value = '';
+})
+
+let link = document.querySelector('a');
+
+link.addEventListener('click', function(event) {
+
+    event.preventDefault();
+
+});
+
+// document.getElementById('check').addEventListener('click', function(event){
+//     console.log(event.target);
+//     console.log(event.currentTarget);
+// })
+
+listContainer.addEventListener('click',(event)=>{
+    console.log(event.target);
+    console.log(event.currentTarget)
+})
+
+let btnCheck = document.getElementById('btnCheck')
+btnCheck.addEventListener('mousemove', (event)=>{
+    console.log('x',event.clientX)
+    console.log("y",event.clientY)
+})
